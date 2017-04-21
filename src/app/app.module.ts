8f539849e5c173adce50ab515626c723f5abed27
QlGitHub <qiannanli@hotmail.com>
2017-04-21 16:11:33 -0400

    Empty with Firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule} from 'angularfire2';
+
+export const firebaseConfig= {
+     apiKey: "AIzaSyBMy2MVkEuBbT8t8GM9pcTZHWf9NnyIues",
+    authDomain: "ionic3-93dde.firebaseapp.com",
+    databaseURL: "https://ionic3-93dde.firebaseio.com",
+    projectId: "ionic3-93dde",
+    storageBucket: "ionic3-93dde.appspot.com",
+    messagingSenderId: "226520731691"
+
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
